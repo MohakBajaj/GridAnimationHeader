aed166688b36b6cf914a188cd9d699fee041a9f7
Mohak Bajaj <bmohak87@gmail.com>
2022-10-11 18:21:15 +0530

    Amazing Grid Animation for Header Project Done! 😍

---
+++ script.js
@@ -0,0 +1,60 @@
+const wrapper = document.getElementById("tiles");
+
+let columns = 0,
+    rows = 0,
+    toggled = false;
+
+const toggle = () => {
+    toggled = !toggled;
+
+    document.body.classList.toggle("toggled");
+}
+
+const handleOnClick = index => {
+    toggle();
+
+    anime({
+        targets: ".tile",
+        opacity: toggled ? 0 : 1,
+        delay: anime.stagger(25, {
+            grid: [columns, rows],
+            from: index
+        })
+    });
+}
+
+const createTile = index => {
+    const tile = document.createElement("div");
+
+    tile.classList.add("tile");
+
+    tile.style.opacity = toggled ? 0 : 1;
+
+    tile.onclick = e => handleOnClick(index);
+
+    return tile;
+}
+
+const createTiles = quantity => {
+    Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+    });
+}
+
+const createGrid = () => {
+    wrapper.innerHTML = "";
+
+    const size = document.body.clientWidth > 800 ? 50 : 25;
+
+    columns = Math.floor(document.body.clientWidth / size);
+    rows = Math.floor(document.body.clientHeight / size);
+
+    wrapper.style.setProperty("--columns", columns);
+    wrapper.style.setProperty("--rows", rows);
+
+    createTiles(columns * rows);
+}
+
+createGrid();
+
+window.onresize = () => createGrid();
